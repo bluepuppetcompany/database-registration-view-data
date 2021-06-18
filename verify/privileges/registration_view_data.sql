f1bc8e868f4ce13edb7ef1ce9fd4b56ee83c0800
Joseph Choe <joseph@josephchoe.com>
2021-06-18 04:59:01 -0600

    Privileges for registration view data

---
+++ verify/privileges/registration_view_data.sql
@@ -0,0 +1,15 @@
+-- Verify database-registration-view-data:privileges/registration_view_data on pg
+
+BEGIN;
+
+SELECT pg_catalog.has_schema_privilege('registration_view_data', 'registration', 'usage');
+
+SELECT pg_catalog.has_table_privilege('registration_view_data', 'registration.registrations', 'select');
+SELECT pg_catalog.has_table_privilege('registration_view_data', 'registration.registrations', 'insert');
+SELECT pg_catalog.has_table_privilege('registration_view_data', 'registration.registrations', 'update');
+SELECT pg_catalog.has_table_privilege('registration_view_data', 'registration.registrations', 'delete');
+
+SELECT pg_catalog.has_sequence_privilege('registration_view_data', 'registration.registrations_global_position_seq', 'usage');
+SELECT pg_catalog.has_sequence_privilege('registration_view_data', 'registration.registrations_global_position_seq', 'select');
+
+ROLLBACK;
